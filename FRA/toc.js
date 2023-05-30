8a0828f3f712dec34e53f12008b4c241c7ca7bbf
domogonzo <112188444+domogonzo@users.noreply.github.com>
2023-05-29 20:02:07 -0400

    Add files via upload

---
+++ FRA/toc.js
@@ -0,0 +1,20 @@
+function toc_content(){ var json = 
+[
+  {
+    "id": 1,
+    "o": 395525,
+    "text": "S'inscrire",
+    "url": "284567.htm",
+    "node": "0",
+    "leaf": true
+  },
+  {
+    "id": 2,
+    "o": 196866,
+    "text": "Mentions l\u00e9gales",
+    "url": "196866.htm",
+    "node": "1",
+    "leaf": true
+  }
+]
+; return json; };
